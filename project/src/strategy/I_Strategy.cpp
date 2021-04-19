72b00ab069fa40be0939c84bdcce661753784271
Conor <brow4339@umn.edu>
2021-04-19 14:43:35 -0500

    #10 removed comments from IStrategy.cpp

--- project/src/strategy/I_Strategy.cpp
+++
@@ -1,6 +0,0 @@
-/*#include "I_Strategy.h"
-
-namespace csci3081 {
-    const std::vector< std::vector<float> > DetermineRoute(const IGraph* graph, const std::vector<float>& src, const std::vector<float>& dest) {
-    }
-}*/
