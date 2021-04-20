9e4e78f15843151690266ef1102c75478f8802d7
Erik Rossing <rossi239@umn.edu>
2021-04-19 19:26:44 -0500

    Add observer pattern discussion

--- project/include/mainpage.h
+++ project/include/mainpage.h
@@ -18,4 +18,13 @@
 * - 4/19/21
 *   - Merged branches into master for final turn-in and completed group documentation
 *
+* \section observer_pattern Observer Pattern Discussion
+* The observer pattern was used to allow the simulations entities to notify observers in
+* the delivery simulation of changes in the state of the entity. For example, when the
+* drone delivers a package and stops moving it will notify the observers of this change.
+* This notification is printed in a UI on the simulations left hand side. The observer
+* pattern was useful because it is dynamic and allows for new observers to be added and
+* removed easily.
+*
+*
 */
